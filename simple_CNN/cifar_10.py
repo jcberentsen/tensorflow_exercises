f51dcba9efba552f698dda18c2c8b2958bc546c0
Dario Cazzani <dcazzani@cisco.com>
2016-10-25 09:27:19 +0200

    Implemented preprocess function

--- simple_CNN/cifar_10.py
+++ simple_CNN/cifar_10.py
@@ -14,6 +14,11 @@ import glob
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 import settings
 import itertools
+from sklearn import preprocessing
+import time
+
+# Helper functions
+from cifar_utils import *
 
 def load_data(folder_name):
 	train_batches_list = glob.glob(folder_name + '/data_batch*')
@@ -29,34 +34,32 @@ def load_data(folder_name):
 	all_labels_test = list(itertools.chain(*all_labels_batches_test))
 	assert(len(all_labels_test) == len(all_images_test))
 
-	return all_labels_train, all_labels_train, all_images_test, all_labels_test
+	return all_images_train, all_labels_train, all_images_test, all_labels_test
 
-def load_batch(filename):
-	fo = open(filename, 'rb')
-	dict = cPickle.load(fo)
-	fo.close()
-	return dict
+def preprocess(data):
+	# perform per image whitening
+	return map(tf.image.per_image_whitening, data)
 
-def extract_plane(raw, index):
-	data = np.asarray(raw[index:index+1024], dtype=np.uint8)
-	return np.reshape(data, (32, 32))
+""" A simple data iterator """
+def data_iterator(data, labels, batch_size):
+  N = data.shape[0] 
+  batch_idx = 0
+  while True:
+    for batch_idx in range(0, N, batch_size):
+      data_batch = data[batch_idx:batch_idx+batch_size]
+      labels_batch = labels[batch_idx:batch_idx+batch_size]
+      yield data_batch, labels_batch
 
-def shape_image(raw):
-	# raw = np.asarray(batch_item, dtype=np.uint8)
-	red = extract_plane(raw,0)
-	green = extract_plane(raw, 1024)
-	blue = extract_plane(raw, 2048)
-	return np.dstack((red, green, blue))
 
-def	load_batch_details(batch_filename):
-	batch = load_batch(batch_filename)
-	return map(shape_image, batch['data']), batch['labels']
 	
 if __name__ == '__main__':
 	folder_name = '/home/dario/Downloads/cifar-10-batches-py'
 	X_train, y_train, X_test, y_test = load_data(folder_name)
-#	plt.imshow(list(all_images)[3])
-#	plt.show()
+	now = time.time()
+	X_train = preprocess(X_train)
+	print time.time() - now
+	#plt.imshow(X_train[3])
+	#plt.show()
 	"""
 	reshaped = np.reshape(data2, (32,32,3), order='A')
 	plt.imshow(reshaped)
